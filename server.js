5854bfa13fe3ab3784e1eacd4f9926490aff2d59
unknown <krishk6@INCSKRISHK6L5C.corp.emc.com>
2015-12-23 14:25:48 +0530

    removed mongodb hardcoding

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ var jsonParser = bodyParser.json()
 
 
 //mongoose.connect('mongodb://localhost:27017/moneyTracker');
-mongoose.connect('mongodb://MyMongo:2nvX_I5j.TI2zgIloVW7o3sv0vEWWINO6KhPPpqxPs8-@ds034198.mongolab.com:34198/MyMongo');
-//mongoose.connect(process.env.DB_URL ||'mongodb://localhost:27017/moneyTracker');
+//mongoose.connect('mongodb://MyMongo:2nvX_I5j.TI2zgIloVW7o3sv0vEWWINO6KhPPpqxPs8-@ds034198.mongolab.com:34198/MyMongo');
+mongoose.connect(process.env.CUSTOM_DB_URL ||'mongodb://localhost:27017/moneyTracker');
 
 /** 
 fs.readdirSync(__dirname+'/app/models').forEach(function(filename){
