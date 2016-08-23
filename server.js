666d5df192d783a0fbc5b790aa817d0f6ef8fb8f
Krishnamurthy <Kumar.Krishnamurthy@emc.com>
2016-08-23 21:29:16 +0530

    removed minification

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ var jsonParser = bodyParser.json()
 //mongoose.connect('mongodb://localhost:27017/moneyTracker');
 //mongoose.connect('mongodb://MyMongo:2nvX_I5j.TI2zgIloVW7o3sv0vEWWINO6KhPPpqxPs8-@ds034198.mongolab.com:34198/MyMongo');
 mongoose.connect(process.env.CUSTOMCONNSTR_DBURL ||'mongodb://localhost:27017/moneyTracker');
+//mongoose.connect(process.env.CUSTOMCONNSTR_DBURL ||'mongodb://kprh2010:Seyon2155$@ds040089.mlab.com:40089/fintrackermlab');
+//mongoose.createConnection(process.env.CUSTOMCONNSTR_DBURL ||'mongodb://kprh2010:Seyon2155$@ds040089.mlab.com:40089/fintrackermlab');
 
 /** 
 fs.readdirSync(__dirname+'/app/models').forEach(function(filename){
