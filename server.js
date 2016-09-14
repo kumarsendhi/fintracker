be8849f81a9a9db91a1478377fdfea61d0f077ed
Krishnamurthy <Kumar.Krishnamurthy@emc.com>
2016-09-14 18:49:54 +0530

    heroku mongo config

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var jsonParser = bodyParser.json()
 //mongoose.connect('mongodb://MyMongo:2nvX_I5j.TI2zgIloVW7o3sv0vEWWINO6KhPPpqxPs8-@ds034198.mongolab.com:34198/MyMongo');
 //mongoose.connect(process.env.CUSTOMCONNSTR_DBURL ||'mongodb://localhost:27017/moneyTracker');
 //mongoose.connect(process.env.CUSTOMCONNSTR_DBURL ||'mongodb://kprh2010:Seyon2155$@ds040089.mlab.com:40089/fintrackermlab');
-mongoose.connect(process.env.CUSTOMCONNSTR_DBURL ||'mongodb://kprh2010:Seyon2155$@ds044229.mlab.com:44229/fintrackermlab');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://kprh2010:Seyon2155$@ds044229.mlab.com:44229/fintrackermlab');
 
 /** 
 fs.readdirSync(__dirname+'/app/models').forEach(function(filename){
